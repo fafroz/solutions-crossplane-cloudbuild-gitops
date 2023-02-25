fd9f0b1e3d34efd819906f6c375acf85ca6711fb
Fahad Afroz <fafroz@dxc.com>
2023-02-25 20:07:42 +0530

    Update create_helm_envsubst_builder.sh

--- scripts/create_helm_envsubst_builder.sh
+++ scripts/create_helm_envsubst_builder.sh
@@ -1,1 +1,14 @@
+git clone https://github.com/GoogleCloudPlatform/cloud-builders-community.git
+cd cloud-builders-community/helm
+# build helm builder & push to conatiner registry
+gcloud builds submit . --config=cloudbuild.yaml
 
+cd ../..
+
+# build envsubst builder & push to conatiner registry
+cd cloud-builders-community/envsubst
+gcloud builds submit . --config=cloudbuild.yaml
+
+cd ../..
+# remove cloud-builders-community
+rm -rf cloud-builders-community
