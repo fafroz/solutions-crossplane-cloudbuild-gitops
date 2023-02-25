eda7b217e8b63d2c5bddff57f6a48bbeee31fd8c
Fahad Afroz <fafroz@dxc.com>
2023-02-25 20:12:53 +0530

    Update create_mgcp_host_crossplane_cluster.sh

--- scripts/create_mgcp_host_crossplane_cluster.sh
+++ scripts/create_mgcp_host_crossplane_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. ./dev/scripts/env_vars.sh
+. ./scripts/crossplaneHost_env_vars.sh
 
 # Activate the service account
 gcloud auth activate-service-account $GCP_SERVICE_ACCOUNT_NAME --key-file=$GCP_SERVICE_ACCOUNT_KEYFILE
