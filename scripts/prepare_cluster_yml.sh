bbe81bd8cb3f559f67dcb986b201711a8179a7ae
Fahad Afroz <fafroz@dxc.com>
2023-02-27 09:44:19 +0530

    Update prepare_cluster_yml.sh

--- scripts/prepare_cluster_yml.sh
+++ scripts/prepare_cluster_yml.sh
@@ -4,5 +4,5 @@
 
 # Use envsubst to replace env var values in the cluster-template YAML template file
 envsubst \
-    < environments/dev/cluster-template.yml \
-    > environments/dev/cluster.yml
+    < templates/cluster-template.yml \
+    > templates/cluster.yml
