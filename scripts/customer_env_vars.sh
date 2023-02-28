aca719e111a7c4d53aa309e963f19804e18b0e2c
Fahad Afroz <fafroz@dxc.com>
2023-03-01 01:00:38 +0530

    Update customer_env_vars.sh

--- scripts/customer_env_vars.sh
+++ scripts/customer_env_vars.sh
@@ -8,7 +8,7 @@ export GCP_SERVICE_ACCOUNT_KEYFILE="gcp_service_account_keyfile"
 export BASE64ENCODED_GCP_PROVIDER_CREDS=$(base64 $GCP_SERVICE_ACCOUNT_KEYFILE | tr -d "\n") # base64 encode the GCP credentials
 
 #customer Environment Variables
-export GKE_CLUSTER_NAME="customer-gke-k8scluster"
+export GKE_CLUSTER_NAME="customer-gke-k8scluster1"
 export GKE_CLUSTER_ZONE="us-central1-c"
 export GKE_NETWORK="gitops-vpc"
 export GKE_SUBNETWORK="gitops-subnet1"
