5e3e73729c124a25abca4100baf127099031ac04
Fahad Afroz <fafroz@dxc.com>
2023-03-01 09:08:15 +0530

    Update crossplaneHost_env_vars.sh

--- scripts/crossplaneHost_env_vars.sh
+++ scripts/crossplaneHost_env_vars.sh
@@ -8,7 +8,7 @@ export GCP_SERVICE_ACCOUNT_KEYFILE="gcp_service_account_keyfile"
 export BASE64ENCODED_GCP_PROVIDER_CREDS=$(base64 $GCP_SERVICE_ACCOUNT_KEYFILE | tr -d "\n") # base64 encode the GCP credentials
 
 #Setting variables for MGCP-Host-crossplane-k8SCluster - CrossplaneHost_env_vars
-export GKE_HOST_CLUSTER_NAME="mgcp-host-crossplane-k8scluster1"
+export GKE_HOST_CLUSTER_NAME="mgcp-host-crossplane-k8scluster"
 export GKE_HOST_CLUSTER_MAX_NODE="1"
 export GKE_HOST_CLUSTER_ZONE="us-central1-c"
 export GKE_HOST_CLUSTER_NETWORK="gitops-vpc"
